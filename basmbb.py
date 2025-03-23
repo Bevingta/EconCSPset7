68c63f26048f6a8e8a34edc211d6c269068c6788
Sawyer Maloney <sawyerdmaloney@gmail.com>
2025-03-23 16:49:12 -0400

    balanced bidding agent

--- basmbb.py
+++ basmbb.py
@@ -5,7 +5,7 @@ import sys
 from gsp import GSP
 from util import argmax_index
 
-class BBAgent:
+class BasmAgent:
     """Balanced bidding agent"""
     def __init__(self, id, value, budget):
         self.id = id
@@ -50,9 +50,33 @@ class BBAgent:
         returns a list of utilities per slot.
         """
         # TODO: Fill this in
+        # compute position effect: pos_1 = c_1/c_1, pos_2 = c_2/c_1, ... pos_m = c_m/c_1
+        # not sure if correct, but at least gives an idea of
+        # how effective each position is wrt to each other
+        clicks = history.rounds[t-1].clicks
+        position_effect = [_ / clicks[0] for _ in clicks]
+        """
+        Calculating utilities: Expected value of pos k is:
+        position_effect[k] * (v_i - min_bid_k)
+        In GSP, we pay min_bid_k (min_bid to get k last round) and we expect value based on 
+        the position effect therein
+        """
+        info = self.slot_info(t, history, reserve)
         utilities = []   # Change this
+        for slot in range(len(info)):
+            # factoring in reserve price, must pay at least rp, otherwise no alloc
+            # so if min_bid > rp, normal
+            # if rp inbetween min & max, we have to at least pay rp
+            # and if rp > max, no alloc and on utility
+            min_bid = info[slot][1]
+            max_bid = info[slot][2]
+            if min_bid > reserve:
+                utilities.append(position_effect[slot] * (self.value - info[slot][1]))
+            elif max_bid > reserve > min_bid:
+                utilities.append(position_effect[slot] * (self.value - reserve))
+            else:  # reserve > max_bid
+                utilities.append(0)  # no allocation
 
-        
         return utilities
 
     def target_slot(self, t, history, reserve):
@@ -82,12 +106,21 @@ class BBAgent:
         (slot, min_bid, max_bid) = self.target_slot(t, history, reserve)
 
         # TODO: Fill this in.
+        # calculating position effects again here, maybe a separate function?
+        clicks = prev_round.clicks
+        position_effect = [_ / clicks[0] for _ in clicks]
         bid = 0  # change this
+
+        """
+            Solving the balanced bidding for b_i^t, given that the price_j is the min_bid, and position effects are calculated before hand.
+        """
+        if slot != 0 and min_bid < self.value:
+            bid = self.value - (position_effect[slot] / position_effect[slot-1]) * (self.value - min_bid)
+        else:  # j == 0 or min_bid > self.value
+            bid = self.value
         
         return bid
 
     def __repr__(self):
         return "%s(id=%d, value=%d)" % (
             self.__class__.__name__, self.id, self.value)
-
-

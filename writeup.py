2558af7e33e5e19571255422d80ce2566d034650
Sawyer Maloney <sawyerdmaloney@gmail.com>
2025-03-23 17:25:52 -0400

    restructing writeup

--- writeup.py
+++ writeup.py
@@ -7,18 +7,27 @@ import sys
 # subprocess.run([])
 
 n = len(sys.argv)
+recognized_arguments = ["3a_truthful", "3a_balanced", "3b_balanced", "3b_truthful"]
 
 if n < 2:
-    print("Command-line argument needed")
+    print("Command-line argument needed. Try:")
+    print(recognized_arguments)
+    sys.exit(1)
 
-recognized_arguments = ["3a_truthful", "3a_balanced"]
 
 if sys.argv[1] not in recognized_arguments:
     print("Unrecoginzed command-line argument. Try: ")
     print(recognized_arguments)
+    sys.exit(1)
 
+command = ""
 if sys.argv[1] == "3a_truthful":
-    subprocess.run("python3 auction.py --perms 1 --iters 200 --seed 2 Truthful,5".split(" "))
-    
+    command = "python3 auction.py --perms 1 --iters 200 --seed 2 Truthful,5"
 elif sys.argv[1] == "3a_balanced":
-    subprocess.run("python3 auction.py --perms 1 --iter 200 --seed 2 Basmbb,5".split(" "))
+    command = "python3 auction.py --perms 1 --iters 200 --seed 2 Basmbb,5"
+elif sys.argv[1] == "3b_balanced":
+    command = "python3 auction.py --perms 10 --iters 100 Truthful,4 Basmbb,1"
+elif sys.argv[1] == "3b_truthful":
+    command = "python3 auction.py --perms 10 --iters 100 Truthful,1 Basmbb,4"
+
+subprocess.run(command.split(" "))
